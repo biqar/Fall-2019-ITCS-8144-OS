a4fe6ac82409001111fa96185cbf730aed1d5c9f
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-08 18:25:35 -0500

    Add code in assignment_2 provided by the instructor

---
+++ assignment_2-allocator/mem_8144.c
@@ -0,0 +1,80 @@
+#include "mem_8144.h"
+
+/* this is just sample code for debugging */
+int test_kmem_id = 0;
+int test_kmem_size = 0;
+pthread_mutex_t test_mutex_lock;
+
+struct mem_ptr {
+  int alloc_size;
+  int alloc_id;
+};
+
+void kmem_init(){
+  /* this is just sample code for debugging */
+  mem_region_ptr = mmap(NULL, MEM_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  if (mem_region_ptr == NULL){
+    perror("memory init error");
+    exit(EXIT_FAILURE);
+  }
+
+  // initilize slabs and data structures
+  pthread_mutex_init(&test_mutex_lock, NULL);
+
+  return;
+}
+
+void *kmalloc_8144(int size){
+  /* this is just sample code for debugging */
+  int local_test_id = 0;
+  struct mem_ptr * ptr = (struct mem_ptr *) malloc(sizeof(struct mem_ptr));
+  
+  pthread_mutex_lock(&test_mutex_lock);
+  test_kmem_id += 1;
+  test_kmem_size += size;
+  local_test_id = test_kmem_id;
+  ptr->alloc_size = size;
+  ptr->alloc_id = test_kmem_id;
+  pthread_mutex_unlock(&test_mutex_lock);
+  
+  printf("[%d] kmalloc %d with size: %d\n", pthread_self(), local_test_id, size);
+  return (void *)ptr;
+}
+
+void kfree_8144(void *ptr){
+  /* this is just sample code for debugging */
+  struct mem_ptr *p = (struct mem_ptr *) ptr;
+  pthread_mutex_lock(&test_mutex_lock);
+  test_kmem_size -= p->alloc_size;
+  pthread_mutex_unlock(&test_mutex_lock);
+  printf("[%d]   kfree %d with size: %d\n", pthread_self(), p->alloc_id, p->alloc_size);
+  return;
+}
+
+void purge_8144(){
+  /* this is just sample code for debugging */
+  printf("  purge memory, system used memory: %d\n", test_kmem_size);
+  return;
+}
+
+int internal_frag(){
+  /* this is just sample code for debugging */
+  return test_kmem_size;
+}
+
+int external_frag(){
+  /* this is just sample code for debugging */
+  return test_kmem_size;
+}
+
+void kmem_finit(){
+  /* this is just sample code for debugging */
+  if (mem_region_ptr != NULL){
+    int r = munmap(mem_region_ptr, MEM_SIZE);
+    if (r == -1){
+      perror("memory finit error");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return;
+}
---
+++ assignment_2-allocator/mem_8144.h
@@ -0,0 +1,25 @@
+#ifndef _MEM_8144_H
+#define _MEM_8144_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define MEM_SIZE 32*1024*1024   /* Total size of the memory region */
+#define BUDDY_MAX_ORDER 10      /* maximal order of buddy algorithm */
+#define SLAB_MIN_ORDER 5        /* minimal object size of a slab area: 32 bytes */ 
+#define SLAB_MAX_ORDER 14       /* maximal object size of a slab area: 2^14 bytes */
+
+static void *mem_region_ptr = NULL;    /* the start of managed memory */
+
+void kmem_init();
+void kmem_finit();
+void *kmalloc_8144(int size);
+void kfree_8144(void *ptr);
+void purge_8144();
+int internal_frag();
+int external_frag();
+
+#endif
