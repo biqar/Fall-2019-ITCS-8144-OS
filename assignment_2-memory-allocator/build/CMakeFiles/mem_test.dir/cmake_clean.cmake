a2b8adf4d7cb58a6b95057ad361e4cdaf66fa7a2
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2019-11-17 20:42:07 -0500

    Add build directory for assignment 2

---
+++ assignment_2-memory-allocator/build/CMakeFiles/mem_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mem_test.dir/mem_8144.c.o"
+  "CMakeFiles/mem_test.dir/mem_test.c.o"
+  "mem_test.pdb"
+  "mem_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mem_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
